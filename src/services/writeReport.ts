945d77e301b0f0084148c91f16ab2ba2e40abd8f
groslapinbleu <antoine.jouannais@gmail.com>
2021-02-10 15:53:44 +0100

    small fix to how backend url is created

--- src/services/writeReport.ts
+++ src/services/writeReport.ts
@@ -6,8 +6,8 @@ interface ReportInterface {
   data: any;
 }
 export const writeReport = (report: ReportInterface): void => {
-  const url = process.env.REACT_APP_ESGR_BACKEND_URL + '/surveys';
+  const url = process.env.REACT_APP_ESGR_BACKEND_URL;
   console.log('ESGR backend url : ' + url);
-  if (url) axios.post(url, report);
+  if (url) axios.post(url + '/surveys', report);
   else console.log("Can't write report to ESGR Backend");
 };
